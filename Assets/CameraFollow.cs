71d4a2d9208506d35eda14740b3099b381f4066f
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-07-22 18:10:01 -0300

    Camera Follow

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] Transform target;
+    Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, 0, target.position.z) + offset, Time.deltaTime * 3); ;
     }
 }
