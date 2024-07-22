68e705616b42244bb652495990903a6bac0dffee
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-07-22 17:34:41 -0300

    Character controller [WIP]

---
+++ Assets/CharacterControllerControls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerControls : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayer;
+
+    public float moveSpeed;
+    public float runningSpeed;
+    private float gravity = -50f;
+    private CharacterController characterController;
+    private Vector3 velocity;
+    private bool isGrounded;
+    float horizontalInput;
+
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        isGrounded = Physics.CheckSphere(transform.position, .1f, groundLayer, QueryTriggerInteraction.Ignore);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+
+        characterController.Move(new Vector3(horizontalInput,0,runningSpeed * Time.deltaTime));
+
+        characterController.Move(velocity*Time.deltaTime);
+    }
+}
