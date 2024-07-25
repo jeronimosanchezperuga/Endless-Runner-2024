ae3c35ab55e5cef65424b96f50d71246cebaf1b2
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-07-25 03:46:37 -0300

    FPS count on UI
    El juego se arrastra a menos de 20 fps, no se la banca y creo que son las texturas. Apenas con 10 módulos, pero con el Postprocessing, hay que decirlo

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text FPSText;
+    private float count;
+    public int screenResolution;
+
+    // Update is called once per frame
+    void Update()
+    {
+        FPSText.text = count.ToString();
+        screenResolution = Screen.currentResolution.refreshRate;
+    }    
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            count = 1f / Time.unscaledDeltaTime;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
