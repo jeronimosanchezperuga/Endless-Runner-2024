0be12da27491794615cda906b432b051ead709a4
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-08-07 00:36:33 -0300

    right turn module added to ground generation

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject groundTile;
+    public GameObject[] groundModules;
     public Transform mapDisplacer;
     public int initialModulesCount = 5;
     public GameObject spawnPoint;
     public bool mapMoves = false;
+    public Vector3 moduleOrientation = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +20,21 @@ public class GroundSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SpawnGroundTile()
     {
-        GameObject clon = Instantiate(groundTile,spawnPoint.transform.position,Quaternion.identity);
+        GameObject groundTile = GetGroundTile();
+        GameObject clon = Instantiate(groundTile, spawnPoint.transform.position, spawnPoint.transform.rotation);
         if (mapMoves) clon.transform.SetParent(mapDisplacer);
+        clon.transform.Rotate(moduleOrientation);
         spawnPoint = clon.transform.GetChild(1).gameObject;
+    }
+
+    private GameObject GetGroundTile()
+    {
+        GameObject chosenTile = groundModules[Random.Range(0,groundModules.Length)];
+        return chosenTile;
     }
 }
