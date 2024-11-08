98dae564ad8aca84b8aea6ce09a3143259253446
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-11-08 10:48:07 -0300

    SentryBot initial navigation and animations

---
+++ Assets/SentryBotNavigation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SentryBotNavigation : MonoBehaviour
+{
+    public Transform targetTR;
+    public Animator anim;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        anim = transform.GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = targetTR.position;
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    }
+
+    public void SetDestination(Transform target)
+    {
+        targetTR = target;
+    }
+}
