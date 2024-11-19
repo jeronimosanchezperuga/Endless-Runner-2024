9584f320f58d5518e3af63e80f37a4e5d5f20f1a
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2024-11-19 12:30:49 -0300

    Interaction script started

---
+++ Assets/RaycastInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastInteraction : MonoBehaviour
+{
+    public Transform rayOrigin;
+    public float rayLenght;
+    public LayerMask layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, rayLenght, layer))
+        {
+            InteractableObject interactable = hit.collider.GetComponent<InteractableObject>();
+            if (interactable)
+            {
+
+            }
+        }
+    }
+}
